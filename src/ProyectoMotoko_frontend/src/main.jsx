da3bd2621dc41630808f713a353af95acccf1275
Mini-Valeria <anavaleriap257@gmail.com>
2024-06-08 22:19:42 -0600

    modified:   package-lock.json 	modified:   src/ProyectoMotoko_frontend/package.json 	new file:   src/ProyectoMotoko_frontend/src/List.jsx 	modified:   src/ProyectoMotoko_frontend/src/main.jsx

--- src/ProyectoMotoko_frontend/src/main.jsx
+++ src/ProyectoMotoko_frontend/src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import List from './List';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>
+  },
+  {
+    path: "/citas-agendadas",
+    element: <List></List>
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
